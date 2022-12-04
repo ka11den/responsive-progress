c13a9393d997ff3214d1c8d0fe1170d5e7d8113d
ka1rex <ka1den@mail.ru>
2022-12-04 14:03:59 +0400

    update v3

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector('#btn'),
+      modal = document.querySelector('#modal'),
+      close = document.querySelector('#close')
+
+btn.addEventListener('click', () => {
+  modal.classList.add('snow')
+})
+
+close.addEventListener('click', () => {
+  modal.classList.remove('snow')
+})
+
+function sendEmail () {
+  let params = {
+    name: document.getElementById('name').value,
+    phone: document.getElementById('phone').value,
+    email: document.getElementById('email').value,
+  }
+
+  const serviceID = 'service_rzx1nv9'
+  const templateID = 'template_rmikegm'
+
+  emailjs
+  .send(serviceID, templateID, params)
+  .then((res) => {
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('phone').value = '';
+      console.log(res);
+      alert('сообщение было успешно отправлено!')
+  })
+  .catch((err) => console.log(err))
+}
